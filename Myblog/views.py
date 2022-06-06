5718f8f6b6b6995b76dceca624867e8b54914ce3
Raphael163 <orbita.route@yandex.ru>
2022-06-06 20:36:20 +0400

    the HTML for the site.

--- Myblog/views.py
+++ Myblog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return render(request, "Myblog/post_list.html")
+
+
+def about(request):
+    return render(request, "Myblog/about.html")
