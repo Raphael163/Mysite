5f2f364e0861c53b9838e0eafdb8ce21956a83e0
Raphael163 <orbita.route@yandex.ru>
2022-06-17 15:46:13 +0400

    LAST 17.06.2022

--- Myblog/urls.py
+++ Myblog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("about", views.about, name='about'),
+    path("create", views.create, name='create'),
     # path("price", views.price, name='price.html'),
     # path("support", views.support, name='support.html')
 ]
--- Myblog/views.py
+++ Myblog/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 from .models import Task
 from .models import Post
+from .forms import TaskForm
 
 
 def index(request):
-    tasks = Task.objects .all()
+    tasks = Task.objects.all()
     return render(request, "Myblog/index.html", {"title": 'Главная страница сайта', 'tasks': tasks})
 
 
 def about(request):
-    posts = Post.objects .all()
+    posts = Post.objects.all()
     return render(request, "Myblog/about.html", {"title": 'Страница о нас', 'posts': posts})
+
+
+def create(request):
+    error = ""
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        else:
+            error = "Форма неверна"
+
+    form = TaskForm
+    context = {
+        "form": form,
+        "error": error
+    }
+    return render(request, "Myblog/create.html", context)
 
 # Подключение шаблонов без модели
 # def index(request):
